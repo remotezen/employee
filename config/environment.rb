dfaa35e4338b91c370d6cc0fdc8ff1ebe2ce423f
remotezen <remotezen@bell.net>
2017-09-05 15:59:06 -0400

    added partials for employee events

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,4 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 Time::DATE_FORMATS[:custom_datetime] = "%a %d %b %Y"
+Time::DATE_FORMATS[:hour_datetime] = "%a %d %b %Y %H %M"
