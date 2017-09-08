b2df6979259cbe0360646c10f7f6eb21e9cf684e
remotezen <remotezen@bell.net>
2017-09-08 10:34:49 -0400

    validates to off.rb

--- app/models/off.rb
+++ app/models/off.rb
@@ -1,5 +1,7 @@
 class Off < ApplicationRecord
   belongs_to :user
   scope  :currently_off, -> {end_time > Time.zone.now }
+  validates :start_time, presence:true
+  validates :end_time, presence:true
 
 end
